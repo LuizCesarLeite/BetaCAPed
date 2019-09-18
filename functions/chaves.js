4cbb45159470de86d64132beba08bcd1407ec0da
LuizCesarLeite <luiz@mentalmarketing.com.br>
2019-09-18 17:55:32 -0300

    Primeiro commit

---
+++ functions/chaves.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyB9yjV5RzcCX4xC7bCVEs5gp1kNoj_IJp0",
+    authDomain: "newagent-48d6b.firebaseapp.com",
+    databaseURL: "https://newagent-48d6b.firebaseio.com",
+    projectId: "newagent-48d6b",
+    storageBucket: "newagent-48d6b.appspot.com",
+    messagingSenderId: "104559969736"
+};
+firebase.initializeApp(config);
