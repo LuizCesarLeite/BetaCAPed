23d464a8cafa5defc933f11181679f9b0c236b02
Luiz Cesar Leite <luiz@mentalmarketing.com.br>
2021-07-20 00:36:58 -0300

    Update chaves.js

--- functions/chaves.js
+++ functions/chaves.js
@@ -1,9 +1,9 @@
 var config = {
-    apiKey: "AIzaSyB9yjV5RzcCX4xC7bCVEs5gp1kNoj_IJp0",
+    apiKey: "---",
     authDomain: "newagent-48d6b.firebaseapp.com",
     databaseURL: "https://newagent-48d6b.firebaseio.com",
     projectId: "newagent-48d6b",
     storageBucket: "newagent-48d6b.appspot.com",
-    messagingSenderId: "104559969736"
+    messagingSenderId: "---"
 };
 firebase.initializeApp(config);
